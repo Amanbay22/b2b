2a6ad2242b5cfea15bca209cff48efdbb10d9b8a
Alex <youdoit@mail.ru>
2021-11-27 09:57:03 -0500

    Added dataframe connection and converting to images

--- api/views.py
+++ api/views.py
@@ -2,11 +2,29 @@ from django.http import response
 from django.shortcuts import render
 from django.http import JsonResponse, HttpResponse
 import requests
-import pprint
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas
+import base64
 # Create your views here.
 
+# TODO:
+# Dataframe
+#
 
-# def index(request):
-# responseData = requests.get("http://api.plos.org/search?q=title:DNA")
-# print(pprint.pformat(responseData.json()))
-# return HttpResponse()
+
+def index(request):
+    responseData = requests.get("http://192.168.0.102:8080/transaction/all")
+    jsonData = responseData.json()
+    df = pandas.json_normalize(jsonData)
+    plt.figure(figsize=(15, 7))
+    sns.barplot(x='dateTime', y='money', data=df,
+                color="mediumaquamarine", ci=None)
+    plt.xticks(rotation=45)
+    plt.savefig('barplot.png')
+    print(pandas.unique(df['companyId']))
+    with open("barplot.png", 'rb') as f:
+
+        byteString = base64.b64encode(f.read())
+    # return HttpResponse()
+    return JsonResponse(jsonData, safe=False)
