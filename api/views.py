6f3dc9d7387ce0bbf581f1cc24a6335fb14910ec
Alex <youdoit@mail.ru>
2021-11-27 11:17:13 -0500

    Added piechart support

--- api/views.py
+++ api/views.py
@@ -14,17 +14,27 @@ import base64
 
 
 def index(request):
-    responseData = requests.get("http://192.168.0.102:8080/transaction/all")
+    responseData = requests.get(
+        "http://192.168.0.110:9999/transaction/all?start=2021-03-27&end=2021-11-24")
     jsonData = responseData.json()
     df = pandas.json_normalize(jsonData)
+    df.sort_values(by=['dateTime'], inplace=True, ascending=True)
     plt.figure(figsize=(15, 7))
     sns.barplot(x='dateTime', y='money', data=df,
                 color="mediumaquamarine", ci=None)
     plt.xticks(rotation=45)
     plt.savefig('barplot.png')
-    print(pandas.unique(df['companyId']))
+    companies = pandas.unique(df['company_name'])
+    piechartData = {}
+    for company in companies:
+        piechartData[company] = len(df.loc[df['company_name'] == company])
+    plt.figure(figsize=(7, 7))
     with open("barplot.png", 'rb') as f:
-
-        byteString = base64.b64encode(f.read())
-    # return HttpResponse()
+        barString = base64.b64encode(f.read())
+    colors = sns.color_palette('pastel')[0:len(companies)]
+    plt.pie(piechartData.values(), labels=companies,
+            colors=colors, autopct='%.0f%%')
+    plt.savefig('pieplot.png')
+    with open("pieplot.png", 'rb') as f:
+        pieString = base64.b64encode(f.read())
     return JsonResponse(jsonData, safe=False)
