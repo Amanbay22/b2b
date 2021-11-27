de770ac2d2aa82740f2f721fbf68ea68f7506157
Alex <youdoit@mail.ru>
2021-11-27 08:08:18 -0500

    Fixed minor bugs and tested functionality

--- api/views.py
+++ api/views.py
@@ -1,3 +1,12 @@
+from django.http import response
 from django.shortcuts import render
-
+from django.http import JsonResponse, HttpResponse
+import requests
+import pprint
 # Create your views here.
+
+
+# def index(request):
+# responseData = requests.get("http://api.plos.org/search?q=title:DNA")
+# print(pprint.pformat(responseData.json()))
+# return HttpResponse()
