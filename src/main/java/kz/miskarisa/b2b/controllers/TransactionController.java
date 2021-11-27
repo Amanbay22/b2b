6f48dbe461b4985408e0531926d619b218287340
saddynsaty <yertayev@kolesa.kz>
2021-11-27 21:41:18 +0600

    formatting

--- src/main/java/kz/miskarisa/b2b/controllers/TransactionController.java
+++ src/main/java/kz/miskarisa/b2b/controllers/TransactionController.java
@@ -1,36 +1,49 @@
 package kz.miskarisa.b2b.controllers;
 
 import kz.miskarisa.b2b.entities.F_Transaction;
+import kz.miskarisa.b2b.repositories.CompanyRepository;
 import kz.miskarisa.b2b.repositories.F_TransactionRepository;
 import kz.miskarisa.b2b.repositories.L_TransactionRepository;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.converter.json.MappingJacksonValue;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("transaction")
 public class TransactionController {
     private F_TransactionRepository f_transactionRepository;
-    private L_TransactionRepository l_transactionRepository;
+    private CompanyRepository companyRepository;
 
-    public TransactionController(F_TransactionRepository f_transactionRepository, L_TransactionRepository l_transactionRepository) {
+    public TransactionController(F_TransactionRepository f_transactionRepository, CompanyRepository companyRepository) {
         this.f_transactionRepository = f_transactionRepository;
-        this.l_transactionRepository = l_transactionRepository;
+        this.companyRepository = companyRepository;
     }
 
     @GetMapping("/all")
-    public MappingJacksonValue getAllTransaction() {
+    public MappingJacksonValue getAllTransaction(@RequestParam("start")
+                                                     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate start,
+                                                 @RequestParam("end")
+                                                 @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate end) {
 
-        List<F_Transaction> transactions = f_transactionRepository.findAll();
+        List<F_Transaction> transactions = f_transactionRepository.findAllByDateTimeBetween(start,end);
+        List<Map<String, Object>> newMap = new ArrayList<>();
+        for (F_Transaction f: transactions) {
+            Map<String, Object> arr = new HashMap<>();
+            arr.put("dateTime", f.getDateTime());
+            arr.put("money", f.getMoney());
+            String name = companyRepository.getById(f.getCompanyRecieverId()).getName();
+            arr.put("company_name", name);
+            newMap.add(arr);
+        }
 
-        MappingJacksonValue jsonT = new MappingJacksonValue(transactions);
-
-        return jsonT;
+        return new MappingJacksonValue(newMap);
     }
-
 
 
 }
