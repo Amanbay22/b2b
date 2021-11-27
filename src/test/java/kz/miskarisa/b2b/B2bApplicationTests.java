e2f3dd45dc8164e365f232b766353a25b35f7f84
saddynsaty <yertayev@kolesa.kz>
2021-11-27 12:11:18 +0600

    first commit

---
+++ src/test/java/kz/miskarisa/b2b/B2bApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.miskarisa.b2b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class B2bApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
